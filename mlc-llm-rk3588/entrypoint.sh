09afd32960aa5fa0cdd5ec1fa3fdec19fb9dac27
vicalloy <zbirder@gmail.com>
2023-09-12 16:02:30 +0800

    add entrypoint.sh to mlc-llm-rk3588/Dockerfile

---
+++ mlc-llm-rk3588/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+cmd=$@
+
+# python3 -m mlc_chat.gradio --artifact-path ARTIFACT_PATH [--device DEVICE] [--port PORT_NUMBER] [--share]
+if [[ $@ == *"mlc_chat.gradio"* ]]; then
+  cmd="${cmd} --device mali --artifact-path dist/prebuilt/"
+fi
+
+# python3 -m mlc_chat.rest --model MODEL [--lib-path LIB_PATH] [--device DEVICE] [--host HOST] [--port PORT]
+if [[ $@ == *"mlc_chat.rest"* ]]; then
+  cmd="${cmd} --device mali  --model ${MODEL_LOCAL_ID}"
+fi
+
+if [[ $@ == "" ]]; then
+  cmd="./build/mlc_chat_cli --device mali --local-id ${MODEL_LOCAL_ID}"
+fi
+
+# echo "${cmd}"
+exec "${cmd}"
